2cd43cd0d4a0d15d76217d09a237d9a6ce4b1ab5
Anton Skorobogatov <anton.skorobogatov@ringcentral.com>
2024-02-19 21:50:12 +0300

    added descriptions for remaps

--- lua/skorobog/remaps.lua
+++ lua/skorobog/remaps.lua
@@ -7,9 +7,8 @@ vim.opt.relativenumber = true
 
 -- Telescope remaps
 local tlscp = require('telescope.builtin') -- I have no
--- I also don't understand what {} are needed here foridea why require is needed here
-vim.keymap.set('n', '<leader>ff', tlscp.find_files, {}) -- find files
-vim.keymap.set('n', '<leader>fl', tlscp.live_grep, {})	--find lines
-vim.keymap.set('n', '<leader>fg', tlscp.git_files, {})	--find git files
-vim.keymap.set('n', '<leader>fb', tlscp.buffers, {})
-vim.keymap.set('n', '<leader>fh', tlscp.help_tags, {})
+vim.keymap.set('n', '<leader>ff', tlscp.find_files, { desc = '[F]ind [f]iles'}) 
+vim.keymap.set('n', '<leader>fl', tlscp.live_grep, { desc = '[F]ind [l]ines'})
+vim.keymap.set('n', '<leader>fg', tlscp.git_files, { desc = '[F]ind [g]it files'})
+vim.keymap.set('n', '<leader>fb', tlscp.buffers, { desc = '[F]ind [b]uffers'})
+vim.keymap.set('n', '<leader>fh', tlscp.help_tags, { desc = '[F]ind [h]elp tags'})
